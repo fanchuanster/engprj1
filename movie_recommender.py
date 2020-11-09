d78d7360366f48cd2310f6a7c0a86e6a42f18de9
Wen Dong <wen.dong@microfocus.com>
2020-11-09 10:24:15 +0200

    tet

--- movie_recommender.py
+++ movie_recommender.py
@@ -15,102 +15,121 @@ logger = logging
 
 DATAST_FOLDRE = 'data/archive'
 
-def get_top_movies_by_score(metadata_df, quantile=0.9):
+class MovieRecommender:
 
-    C = metadata_df['vote_average'].mean()
-    m = metadata_df['vote_count'].quantile(quantile)
+    def __init__(self):
+        self.metadata_df = pd.read_csv(DATAST_FOLDRE+"/movies_metadata.csv", low_memory=False)
+        self.top5000_movies = self.get_top_movies_by_score()
 
-    movies_df = metadata_df.copy()[metadata_df['vote_count'] >= m]
+    def get_top_movies_by_score(self, quantile=0.9):
 
-    def weighted_rating(row, m=m, C=C):
-        v = row['vote_count']
-        r = row['vote_average']
-        return v/(v+m) * r + m/(m+v) * C
+        C = self.metadata_df['vote_average'].mean()
+        m = self.metadata_df['vote_count'].quantile(quantile)
 
-    movies_df['score'] = movies_df.apply(weighted_rating, axis=1)
-    movies_df = movies_df.sort_values(['score'], ascending=False)
+        movies_df = self.metadata_df.copy()[self.metadata_df['vote_count'] >= m]
 
-    return movies_df[['id', 'title', 'vote_count', 'vote_average', 'score', 'overview']]
+        def weighted_rating(row, m=m, C=C):
+            v = row['vote_count']
+            r = row['vote_average']
+            return v/(v+m) * r + m/(m+v) * C
 
-def get_similar_movies_by_overview(top5000_movies, metadata_df, title):
+        movies_df['score'] = movies_df.apply(weighted_rating, axis=1)
+        movies_df = movies_df.sort_values(['score'], ascending=False)
 
-    movies_df = metadata_df.copy()[metadata_df['id'].isin(top5000_movies.id) | (metadata_df['title']==title)]
-    search_movie = movies_df[movies_df['title']==title].iloc[-1]
-    idx = movies_df.index.get_loc(search_movie.name)
+        return movies_df[['id', 'title', 'vote_count', 'vote_average', 'score', 'overview']]
 
-    tfidf = TfidfVectorizer(stop_words='english')
-    movies_df['overview'].fillna('', inplace=True)
+    def get_similar_movies_by_overview(self, title):
 
-    matrix = tfidf.fit_transform(movies_df['overview'])    
-    cosine_sim = linear_kernel(matrix, matrix)
-    
-    scores = list(enumerate(cosine_sim[idx]))
-    movies_df['score_'] = movies_df.apply(lambda row:scores[movies_df.index.get_loc(row.name)][1], axis=1)
-    movies_df.sort_values(['score_'], ascending=False, inplace=True)
-    return movies_df[['title', 'score_']].iloc[0 : 11]
+        movies_df = self.metadata_df.copy()[self.metadata_df['id'].isin(self.top5000_movies.id) | (self.metadata_df['title']==title)]
+        movie_to_searchs = movies_df[movies_df['title']==title]
+        if movie_to_searchs.empty:
+            return None
+
+        assert len(movie_to_searchs.index)==1, "Should be only one movie matching movie title '%s'" % movie_to_searchs
+
+        movie_to_search = movie_to_searchs.iloc[-1]
+        idx = movies_df.index.get_loc(movie_to_search.name)
+
+        tfidf = TfidfVectorizer(stop_words='english')
+        movies_df['overview'].fillna('', inplace=True)
+
+        matrix = tfidf.fit_transform(movies_df['overview'])    
+        cosine_sim = linear_kernel(matrix, matrix)
+        
+        scores = list(enumerate(cosine_sim[idx]))
+        movies_df['score_'] = movies_df.apply(lambda row:scores[movies_df.index.get_loc(row.name)][1], axis=1)
+        movies_df.sort_values(['score_'], ascending=False, inplace=True)
+        return movies_df[['title', 'score_']].iloc[0 : 6]
 
 
-def get_similar_by_features(top5000, metadata_df, movie_title):
-    keywords = pd.read_csv(DATAST_FOLDRE+'/keywords.csv', dtype={'id':'Int64'})
-    credits = pd.read_csv(DATAST_FOLDRE+'/credits.csv', dtype={'id':'Int64'})
-    metadata_df = metadata_df.drop([19730, 29503, 35587])
-    metadata_df['id'] = metadata_df['id'].astype('int')
+    def get_similar_by_features(self, metadata_df, movie_title):
+        keywords = pd.read_csv(DATAST_FOLDRE+'/keywords.csv', dtype={'id':'Int64'})
+        credits = pd.read_csv(DATAST_FOLDRE+'/credits.csv', dtype={'id':'Int64'})
+        metadata_df = metadata_df.drop([19730, 29503, 35587])
+        metadata_df['id'] = metadata_df['id'].astype('int')
 
-    print(metadata_df.shape)
-    metadata_df = metadata_df.merge(keywords, how='inner', on='id').merge(credits, how='inner', on='id')
-    print(metadata_df.shape)
+        # print(metadata_df.shape)
+        metadata_df = metadata_df.merge(keywords, how='inner', on='id').merge(credits, how='inner', on='id')
+        # print(metadata_df.shape)
 
-    features = ['cast', 'crew', 'keywords', 'genres']
-    for feature in features:
-        # metadata_df[feature] = metadata_df[feature].apply(lambda f:literal_eval(f[feature].lower() if f[feature] else ""), axis=1)
-        metadata_df[feature] = metadata_df[feature].apply(literal_eval)
-        print(metadata_df[feature].head(5))
-    
-    def get_director(x):
-        for i in x:
-            if 'director' in i['job'].lower():
-                return i['name'].lower().replace(" ", "")
-        return np.nan
+        features = ['cast', 'crew', 'keywords', 'genres']
+        for feature in features:
+            # metadata_df[feature] = metadata_df[feature].apply(lambda f:literal_eval(f[feature].lower() if f[feature] else ""), axis=1)
+            metadata_df[feature] = metadata_df[feature].apply(literal_eval)
+            print(metadata_df[feature].head(5))
+        
+        def get_director(x):
+            for i in x:
+                if 'director' in i['job'].lower():
+                    return i['name'].lower().replace(" ", "")
+            return np.nan
 
-    metadata_df['director'] = metadata_df['crew'].apply(get_director)
+        metadata_df['director'] = metadata_df['crew'].apply(get_director)
 
-    def get_list(x):
-        if isinstance(x, list):
-            names = [i['name'].lower().replace(" ", "") for i in x]
-            #Check if more than 3 elements exist. If yes, return only first three. If no, return entire list.
-            return names[:3]
-        return []
+        def get_list(x):
+            if isinstance(x, list):
+                names = [i['name'].lower().replace(" ", "") for i in x]
+                #Check if more than 3 elements exist. If yes, return only first three. If no, return entire list.
+                return names[:3]
+            return []
 
-    features = ['cast', 'keywords', 'genres']
-    for feature in features:
-        metadata_df[feature] = metadata_df[feature].apply(get_list)
+        features = ['cast', 'keywords', 'genres']
+        for feature in features:
+            metadata_df[feature] = metadata_df[feature].apply(get_list)
 
-    # print(metadata_df[['title', 'cast', 'director', 'keywords', 'genres']].head(3))
+        # print(metadata_df[['title', 'cast', 'director', 'keywords', 'genres']].head(3))
 
-    def create_soup(x):
-        return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + x['director'] + ' ' + ' '.join(x['genres'])
-    # Create a new soup feature
-    metadata_df['soup'] = metadata_df.apply(create_soup, axis=1)
-    metadata_df[['soup']].head(2)
+        def create_soup(x):
+            return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + x['director'] + ' ' + ' '.join(x['genres'])
+        # Create a new soup feature
+        metadata_df['soup'] = metadata_df.apply(create_soup, axis=1)
+        metadata_df[['soup']].head(2)
 
-    count = CountVectorizer(stop_words='english')
-    count_matrix = count.fit_transform(metadata_df['soup'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    metadata_df = metadata_df.reset_index()
+        count = CountVectorizer(stop_words='english')
+        count_matrix = count.fit_transform(metadata_df['soup'])
+        cosine_sim = cosine_similarity(count_matrix, count_matrix)
+        metadata_df = metadata_df.reset_index()
 
 
 def main():
-    metadata_df = pd.read_csv(DATAST_FOLDRE+"/movies_metadata.csv", low_memory=False)
     
     # pd.set_option("display.max_rows", None, "display.max_columns", None, "display.width", 1000)
 
-    top5000 = get_top_movies_by_score(metadata_df)
-    print(top5000.shape)
+    mr = MovieRecommender()
 
-    # recommendations = get_similar_movies_by_overview(top5000, metadata_df, 'Grumpier Old Men')
-    recommendations = get_similar_by_features(top5000, metadata_df, 'Grumpier Old Men')
-    
-    print(recommendations)
+    quit = False
+    while (not quit):
+        print("Input the movie title for recommendations like 'Grumpier Old Men', or 'quit' to quit:")
+        title = input()
+        title.strip("' ")
+        if title == 'quit':
+            quit = True
+            continue
+
+        recommendations = mr.get_similar_movies_by_overview(title)
+        # recommendations = get_similar_by_features(top5000_movies, metadata_df, 'Grumpier Old Men')
+        
+        print(recommendations)
 
 
 if __name__ == '__main__':
