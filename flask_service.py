3d1bb24991f519d252e245a2c4d32128d8472de7
Wen Dong <wen.dong@microfocus.com>
2020-11-18 09:10:32 +0200

    add flask service

---
+++ flask_service.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from mvie_recommender import MovieRecommender
+import json
+
+app = Flask(__name__)
+
+@app.route("/recommend/<string:movie_title>")
+def recommend(movie_title):
+    mr = MovieRecommender()
+    recommendations = mr.recommend_by_overview(title)
+    return json.dumps(recommendations)
+
+if __name__ == "__main__":
+    app.run(debug=True)
